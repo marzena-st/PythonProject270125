3fa7c03e065cbd9eea12017b549bad09a2fc72e8
marzena-st <marzena.starzyk@onet.eu>
2025-01-28 09:22:57 +0100

    init commit

---
+++ main.py
@@ -0,0 +1,25 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# 2 x shift - podręczne menu
+# wheel, theme
+# PEP8 https://peps.python.org/pep-0008/
+# zasady stylistyczne pisania kodu
+# ctrl alt l - formatowanie kodu do zasad PEP8
+# (.venv) PS C:\Users\CSComarch\PycharmProjects\PythonProject270125> python .\main.py - uruchomienie skryptu w terminalu
+# Hi, PyCharm
+# ctrl / - automatyczny komentarz
+
